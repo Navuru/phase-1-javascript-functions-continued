90cc77f97f9bff8b01c369f3bc68411b2421ec0d
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-08-19 18:35:05 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective (param = "*") {
+    return function (adjective = "special") {
+        return `You are ${param}${adjective}${param}!`
+    }
+}
